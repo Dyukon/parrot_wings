941afd59dcaaad02e55d4336c12f6fe2424c20cc
Denis Konovalchik <dyukon@gmail.com>
2022-11-23 09:39:44 +0300

    use configuration from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TransactionModule } from './transaction/transaction.module';
-import {ConfigModule} from "@nestjs/config"
+import {ConfigModule, ConfigService} from '@nestjs/config'
 import {TypeOrmModule} from '@nestjs/typeorm'
+import {User} from './user/user.entity'
+import {Transaction} from './transaction/transaction.entity'
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      host: 'localhost',
-      port: 27017,
-      username: 'admin',
-      password: 'admin',
-      database: 'admin',
-      ssl: false,
-      autoLoadEntities: true,
-      useUnifiedTopology: true
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mongodb',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [User, Transaction],
+        synchronize: true,
+      }),
     }),
     UserModule,
     TransactionModule
